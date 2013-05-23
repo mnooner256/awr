6b841543c31e14f1bdb470088247f750043dcb4a
Anna Shafer <ashafer1@harding.edu>
2013-05-23 18:49:47 -0500

    RFID rough output

--- AnnaHW/main.cpp
+++ AnnaHW/main.cpp
@@ -1,20 +1,44 @@
-//Testing
 #include <Arduino.h>
+#include <SoftwareSerial\SoftwareSerial.h>
 
-int ledPin =  13;
-
+SoftwareSerial id20(3,2); // virtual serial port
+char i;
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+	 Serial.begin(9600);
+	 id20.begin(9600);
 }
 
-void loop()
-{
-  digitalWrite(ledPin, HIGH);
-  delay(5000);
-  digitalWrite(ledPin, LOW);
-  delay(5000);
+void loop(){
+
+	if(id20.available()) {
+	 i = id20.read(); // receive character from ID20
+	 Serial.print(i); // send character to serial monitor
+	 Serial.print(" ");
+	}
+
+//  char tagString[13];
+//  int index = 0;
+//  boolean reading = false;
+//
+//  while(Serial.available()){
+//
+//	Serial.println("Tag present");
+//    int readByte = Serial.read(); //read next available byte
+//
+//    if(readByte == 2) reading = true; //beginning of tag
+//    if(readByte == 3) reading = false; //end of tag
+//
+//    if(reading && readByte != 2 && readByte != 10 && readByte != 13){
+//      //store the tag
+//      tagString[index] = readByte;
+//      index ++;
+//    }
+//    Serial.println(tagString); //read out any unknown tag
+//  }
+
+  //Serial.println("No tag present");
 }
 
 int main(void)
