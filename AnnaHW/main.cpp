0b1b8b7dc329937ebf45242b05d15db85e7d4ccf
Anna Shafer <ashafer1@harding.edu>
2013-05-23 12:39:40 -0500

    Intial Project Commit (agian)

---
+++ AnnaHW/main.cpp
@@ -0,0 +1,28 @@
+//Testing
+#include <Arduino.h>
+
+int ledPin =  13;
+
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
+
+int main(void)
+{
+  init();
+  setup();
+
+  while(true) {
+    loop();
+  }
+}
