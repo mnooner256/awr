2d10b954bdaaac02636d1461d833eab89dd51413
Michael Nooner <mnooner256@gmail.com>
2013-05-23 09:15:49 -0500

    Removed comments

--- ArduinoTemplate/main.cpp
+++ ArduinoTemplate/main.cpp
@@ -1,25 +1,19 @@
 #include <Arduino.h>
 
-int ledPin =  13;    // LED connected to digital pin 13
+int ledPin =  13;
 
-// The setup() method runs once, when the sketch starts
+
 void setup()
 {
   Serial.begin(115200);
 }
 
-// the loop() method runs over and over again,
-// as long as the Arduino has power
 void loop()
 {
   Serial.println("Hello, World!");
   delay(1000);
 }
 
-// just like any application, this is the entry point
-// init is required to initialize the Arduino library
-// we then just call setup(), followed loop() inside
-// an infinite loop.
 int main(void)
 {
   init();
