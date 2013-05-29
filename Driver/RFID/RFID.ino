848bf82f59d324eae950a5b7518908de60307dea
Anna Shafer <ashafer1@harding.edu>
2013-05-28 19:02:30 -0500

    Storing correct ID string

--- Driver/RFID/RFID.ino
+++ Driver/RFID/RFID.ino
@@ -1,44 +1,29 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial id20(3,2); // virtual serial port
+SoftwareSerial id20(3,2); // virtual serial port(RX,T
 char i;
 
 void setup()
 {
-	 Serial.begin(9600);
-	 id20.begin(9600);
+  Serial.begin(9600);
+  id20.begin(9600);
 }
 
 void loop(){
-
-	if(id20.available()) {
-	 i = id20.read(); // receive character from ID20
-	 Serial.print(i); // send character to serial monitor
-	 Serial.print(" ");
-	}
-
-//  char tagString[13];
-//  int index = 0;
-//  boolean reading = false;
-//
-//  while(Serial.available()){
-//
-//	Serial.println("Tag present");
-//    int readByte = Serial.read(); //read next available byte
-//
-//    if(readByte == 2) reading = true; //beginning of tag
-//    if(readByte == 3) reading = false; //end of tag
-//
-//    if(reading && readByte != 2 && readByte != 10 && readByte != 13){
-//      //store the tag
-//      tagString[index] = readByte;
-//      index ++;
-//    }
-//    Serial.println(tagString); //read out any unknown tag
-//  }
-
-  //Serial.println("No tag present");
+  char tagString[13] = {0};  //Initialize ID tag string to zero
+  
+  if(id20.available() ) {
+    for( int index = 0; index < 12; index++) {
+    i = id20.read(); // receive character from ID20
+    tagString[index] = i;
+    }
+  }
+    
+  if(tagString != 0) {
+    Serial.println(tagString); // send ID# to serial monitor
+  }
+  
 }
 
 int main(void)
