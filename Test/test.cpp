dc89230389d23f1b3050bb07c99d4f2fbf5071b7
Anna Shafer <ashafer1@harding.edu>
2013-05-30 13:28:48 -0500

    COM4 as port for serial communication on Arduino

--- Test/test.cpp
+++ Test/test.cpp
@@ -5,6 +5,23 @@
  *      Author: Anna
  */
 
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	HANDLE ard;
+	ard = CreateFile("COM4",GENERIC_READ | GENERIC_WRITE,
+			0,0,OPEN_EXISTING,FILE_FLAG_OVERLAPPED,0);
+
+	if(ard == INVALID_HANDLE_VALUE) {
+		cout << "Could not open new port.";
+		return 1;
+	}
+	else cout<< "Success!";
 
 
+	return 0;
+}
 
