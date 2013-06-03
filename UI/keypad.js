56ec5a7ea2404359e02d55afebd0f4efd2ca0ef0
JustinCMabon <JustinCMabon@161.31.73.234>
2013-06-03 09:03:17 -0500

    Post code review. Cleaned up a bunch of stuff. The clear button now works.

--- UI/keypad.js
+++ UI/keypad.js
@@ -1,35 +1,17 @@
-/* comment 
-goes 
-here
-*/
 
- function open(){
-	alert("Welcome (/^_^)/");
-}
-open();
- 
- function showNumber(num){
+function showNumber(num){
    var tbox = document.getElementById("rooms");
    tbox.value = tbox.value+num;
-   total = total + num;
- }
+}
  
- function showTotal(){
-   alert(total);
- }
- 
- 
-var num0 = 0;
-var num1 = 1;
-var num2 = 2;
-var num3 = 3;
-var num4 = 4;
-var num5 = 5;
-var num6 = 6;
-var num7 = 7;
-var num8 = 8;
-var num9 = 9;
-var Cler = "Clr";
-var ent = "ACS";
+function showTotal(){
+   var tbox = document.getElementById("rooms");
+   
+   alert(tbox.value);
+   tbox.value = "";
+}
 
-
+function clearBox() {
+	var tbox = document.getElementById("rooms");
+	tbox.value = "";
+}
