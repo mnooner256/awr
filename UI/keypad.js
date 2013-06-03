bc674b152b90994c709af095e338348aad462cc9
JustinCMabon <JustinCMabon@161.31.73.234>
2013-06-03 08:43:27 -0500

    keypad.html keypad.js global.js
    all files work together to make up the keypad, still needs a few changes

---
+++ UI/keypad.js
@@ -0,0 +1,44 @@
+/* comment 
+goes 
+here
+*/
+
+ function open(){
+	alert("Welcome (/^_^)/");
+ }
+ open();
+
+ function x(num7){
+ alert(num7)
+	 }
+ x();
+ 
+ function num8(){
+	("8");
+ }
+ num8();
+ 
+ function showNumber(num){
+   var tbox = document.getElementById("rooms");
+   tbox.value = tbox.value+num;
+   total = total + num;
+ }
+ function showTotal(){
+   alert(total);
+ }
+ 
+ 
+var num0 = 0;
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+var num6 = 6;
+var num7 = 7;
+var num8 = 8;
+var num9 = 9;
+var Cler = "Clr";
+var ent = "ACS";
+
+
