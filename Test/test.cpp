7fe11c854c2e3bd529da7e497a2ad7e8393d751d
Anna Shafer <ashafer1@harding.edu>
2013-06-04 17:03:31 -0500

    Write not working. Event management needs work.

--- Test/test.cpp
+++ Test/test.cpp
@@ -11,15 +11,13 @@
 
 int main()
 {
-
+	BOOL fWaitingOnRead = TRUE;
+	BOOL writeResult = FALSE;
 	const int dataLength = 256;
 	int readResult = 0;
 	char buf[dataLength] = "";
 
-	// application reads from the specified serial port and reports the collected data
-	printf("Welcome to the serial test app!\n\n");
-
-	Serial* SP = new Serial("COM4");    // adjust as needed
+	Serial* SP = new Serial("COM4");
 
 	if (SP->IsConnected())
 		printf("We're connected\n");
@@ -38,44 +36,24 @@ int main()
 	{
 		char buf[dataLength] = "";
 
-		readResult = SP->ReadData(buf,dataLength, osReader);
-		Sleep(50);			//Wait for communication to initialize
-		//printf("Bytes read: (-1 means no data available) %i\n",readResult);
-		std::cout <<buf;
+		if(fWaitingOnRead) {
+			readResult = SP->ReadData(buf,dataLength, osReader);
+			Sleep(50);			//Wait for communication to initialize
+			//printf("Bytes read: (-1 means no data available) %i\n",readResult);
+			std::cout <<buf;
+
+			if(readResult >= 12 ) fWaitingOnRead = FALSE;
+		}
+		if(!fWaitingOnRead) {
+			std::cout <<"What would you like to send? ";
+			std::cin >> buf[0];
+			writeResult = SP->WriteData(buf,1,osReader);
+			if(!writeResult) {
+				std::cout <<"Could not write to serial port.\n";
+			}
+			else fWaitingOnRead = TRUE;
+		}
 	}
+
 	return 0;
 }
-
-//DWORD dwRead;
-//BOOL fWaitingOnRead = FALSE;
-//OVERLAPPED osReader = {0};
-
-//	// Create the overlapped event. Must be closed before exiting
-//	// to avoid a handle leak.
-//	osReader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-//
-//	if (osReader.hEvent == NULL) {
-//	   // Error creating overlapped event; abort.
-//		CloseHandle(serialArd);
-//		return 1;
-//	}
-//
-//	while (!fWaitingOnRead) {
-//		//for (char c : buf){ c = NULL; }
-//
-//		// Issue read operation.
-//		if (!ReadFile(serialArd, buf, MAX_BUF_SIZE-1, &dwRead, &osReader)) {
-//			  if (GetLastError() != ERROR_IO_PENDING) {    // read not delayed?
-//				 // Error in communications; report it.
-//				  std::cout << "Could not read from port.\n";
-//				  CloseHandle(serialArd);
-//				  return 1;
-//			  }
-//			  else
-//				 fWaitingOnRead = TRUE;
-//		 }
-//		 else {
-//		  // read successful
-//			std::cout << buf;
-//		}
-
