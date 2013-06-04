4c849e17c153527801976ff98badbfc9b669d04c
Anna Shafer <ashafer1@harding.edu>
2013-06-04 13:07:45 -0500

    Ready to test reading

---
+++ Test/serial.h
@@ -0,0 +1,40 @@
+/*
+ * serial.h
+ *
+ *  Created on: Jun 4, 2013
+ *      Author: Anna
+ */
+
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+#include <Windows.h>
+
+class Serial
+{
+    private:
+        HANDLE serialArd;	//Serial comm handler
+        bool connected;		//Connection status
+        COMSTAT status;		//Get various information about the connection
+        DWORD errors;		//Keep track of last error
+
+    public:
+        //Initialize Serial communication with the given COM port
+        Serial(char *portName);
+        //Close the connection
+        ~Serial();
+        //Read data in a buffer, if nbChar is greater than the
+        //maximum number of bytes available, it will return only the
+        //bytes available. The function return -1 when nothing could
+        //be read, the number of bytes actually read.
+        int ReadData(char *buffer, unsigned int nbChar);
+        //Writes data from a buffer through the Serial connection
+        //return true on success.
+        bool WriteData(char *buffer, unsigned int nbChar);
+        //Check if we are actually connected
+        bool IsConnected();
+
+
+};
+
+#endif /* SERIAL_H_ */
--- Test/test.cpp
+++ Test/test.cpp
@@ -5,10 +5,69 @@
  *      Author: Anna
  */
 
-#include <serial.h>
 #include <iostream>
+#include "serial.h"
+#include <Windows.h>
 
 int main()
 {
+	const int dataLength = 256;
+	int readResult = 0;
+	char buf[dataLength] = "";
+
+	// application reads from the specified serial port and reports the collected data
+	printf("Welcome to the serial test app!\n\n");
+
+	Serial* SP = new Serial("\\\\.\\COM4");    // adjust as needed
+
+	if (SP->IsConnected())
+		printf("We're connected");
+
+	while(SP->IsConnected())
+	{
+		readResult = SP->ReadData(buf,dataLength);
+		printf("Bytes read: (-1 means no data available) %i\n",readResult);
+
+		std::string test(buf);
+
+		printf("%s",buf);
+		test = "";
+
+		Sleep(500);
+	}
+	return 0;
 }
+
+//DWORD dwRead;
+//BOOL fWaitingOnRead = FALSE;
+//OVERLAPPED osReader = {0};
+
+//	// Create the overlapped event. Must be closed before exiting
+//	// to avoid a handle leak.
+//	osReader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+//
+//	if (osReader.hEvent == NULL) {
+//	   // Error creating overlapped event; abort.
+//		CloseHandle(serialArd);
+//		return 1;
+//	}
+//
+//	while (!fWaitingOnRead) {
+//		//for (char c : buf){ c = NULL; }
+//
+//		// Issue read operation.
+//		if (!ReadFile(serialArd, buf, MAX_BUF_SIZE-1, &dwRead, &osReader)) {
+//			  if (GetLastError() != ERROR_IO_PENDING) {    // read not delayed?
+//				 // Error in communications; report it.
+//				  std::cout << "Could not read from port.\n";
+//				  CloseHandle(serialArd);
+//				  return 1;
+//			  }
+//			  else
+//				 fWaitingOnRead = TRUE;
+//		 }
+//		 else {
+//		  // read successful
+//			std::cout << buf;
+//		}
 
