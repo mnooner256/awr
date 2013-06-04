26766daffb5e568c72e87ee6b9287365528a54cb
Anna Shafer <ashafer1@harding.edu>
2013-06-04 13:40:01 -0500

    not sure why this wasn't already committed

--- Test/test.cpp
+++ Test/test.cpp
@@ -5,77 +5,69 @@
  *      Author: Anna
  */
 
-#include <Windows.h>
 #include <iostream>
+#include "serial.h"
+#include <Windows.h>
 
 int main()
 {
-	HANDLE serialArd;
-	DWORD dwRead;
-	BOOL fWaitingOnRead = FALSE;
-	OVERLAPPED osReader = {0};
-	const int MAX_BUF_SIZE = 256;
-	char buf[MAX_BUF_SIZE];
-	DCB dcb = {0};
+	const int dataLength = 256;
+	int readResult = 0;
+	char buf[dataLength] = "";
 
-	//opening an overlapping I/O port
-	serialArd = CreateFile("COM4",GENERIC_READ | GENERIC_WRITE,
-			0,0,OPEN_EXISTING,FILE_FLAG_OVERLAPPED,0);
+	// application reads from the specified serial port and reports the collected data
+	printf("Welcome to the serial test app!\n\n");
 
-	if(serialArd == INVALID_HANDLE_VALUE) {
-		std::cout << "Could not open new port.\n";
-		return 1;
+	Serial* SP = new Serial("COM4");    // adjust as needed
+
+	if (SP->IsConnected())
+		printf("We're connected");
+
+	while(SP->IsConnected())
+	{
+		readResult = SP->ReadData(buf,dataLength);
+		printf("Bytes read: (-1 means no data available) %i\n",readResult);
+
+		std::string test(buf);
+
+		printf("%s",buf);
+		test = "";
+
+		Sleep(500);
 	}
-	else {
-	    dcb.DCBlength = sizeof(dcb);
-	    if (!::GetCommState(serialArd, &dcb)) {
-	    	std::cout << "Could not get communication state.\n";
-	    	return 1;
-	    }
-	    dcb.BaudRate    = 9600;
-	    dcb.ByteSize    = 8;
-	    dcb.Parity      = NOPARITY;
-	    dcb.StopBits    = ONESTOPBIT;
-
-	    if (!::SetCommState (serialArd,&dcb)) {
-	    	std::cout << "Could not set communication state.\n";
-	    	return 1;
-	    }
-
-
-		// Create the overlapped event. Must be closed before exiting
-		// to avoid a handle leak.
-		osReader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-
-		if (osReader.hEvent == NULL) {
-		   // Error creating overlapped event; abort.
-			CloseHandle(serialArd);
-			return 1;
-		}
-
-		if (!fWaitingOnRead) {
-			// Issue read operation.
-			if (!ReadFile(serialArd, buf, MAX_BUF_SIZE-1, &dwRead, &osReader)) {
-				  if (GetLastError() != ERROR_IO_PENDING) {    // read not delayed?
-					 // Error in communications; report it.
-					  std::cout << "Could not read from port.\n";
-					  CloseHandle(serialArd);
-					  return 1;
-				  }
-				  else
-					 fWaitingOnRead = TRUE;
-			 }
-			 else {
-			  // read completed immediately
-				 std::cout << "Successfully read from port.\n";
-				 for(int i=0; i<MAX_BUF_SIZE; i++) {
-					 std::cout << buf[i];
-				 }
-			}
-		}
-	}
-
-	CloseHandle(serialArd);
 	return 0;
 }
+
+//DWORD dwRead;
+//BOOL fWaitingOnRead = FALSE;
+//OVERLAPPED osReader = {0};
+
+//	// Create the overlapped event. Must be closed before exiting
+//	// to avoid a handle leak.
+//	osReader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+//
+//	if (osReader.hEvent == NULL) {
+//	   // Error creating overlapped event; abort.
+//		CloseHandle(serialArd);
+//		return 1;
+//	}
+//
+//	while (!fWaitingOnRead) {
+//		//for (char c : buf){ c = NULL; }
+//
+//		// Issue read operation.
+//		if (!ReadFile(serialArd, buf, MAX_BUF_SIZE-1, &dwRead, &osReader)) {
+//			  if (GetLastError() != ERROR_IO_PENDING) {    // read not delayed?
+//				 // Error in communications; report it.
+//				  std::cout << "Could not read from port.\n";
+//				  CloseHandle(serialArd);
+//				  return 1;
+//			  }
+//			  else
+//				 fWaitingOnRead = TRUE;
+//		 }
+//		 else {
+//		  // read successful
+//			std::cout << buf;
+//		}
 
