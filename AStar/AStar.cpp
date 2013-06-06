955349e3ed0e2811486dc3455597af972b55a43c
Francisco Ruiz <frankruiz3@hotmail.com>
2013-06-05 21:48:54 -0500

    Finished getMap section of code...continuing on

--- AStar/AStar.cpp
+++ AStar/AStar.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <fstream>
+#include <string>
 #include "Node.h"
 
 using namespace std;
@@ -13,6 +14,7 @@ void getMap(Node map[])
 {
 	fstream f;
 	int x_size, y_size, total_size;
+	string dummy;
 
 	f.open("c:/Users/Francisco/Desktop/map_layout.txt", ios::in);
 
@@ -24,6 +26,14 @@ void getMap(Node map[])
 	//loops through the map and reads values to node
 	for(int i = 0; i < total_size; i++)
 	{
+		f >> map[i].xPos >> map[i].yPos >> map[i].rfid >> dummy;
+		//will check for garbage and collect if necessary
+		while(dummy != '\n')
+		{
+			f >> dummy;
+		}
+	}
+}
 
 	}
 }
