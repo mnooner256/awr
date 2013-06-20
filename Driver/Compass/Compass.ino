0f90abd8196cfa4e44959a12979ec6c168805129
Anna Shafer <ashafer1@harding.edu>
2013-06-19 21:03:41 -0500

    Compass readings in degrees relative to starting/initial heading. Currently using buffer of size 5 to average out readings. Problem with rollover and, if delay is used, keeping up with fast movements.

--- Driver/Compass/Compass.ino
+++ Driver/Compass/Compass.ino
@@ -1,18 +1,4 @@
 /*
-HMC5883L_Example.pde - Example sketch for integration with an HMC5883L triple axis magnetomerwe.
-Copyright (C) 2011 Love Electronics (loveelectronics.co.uk)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the version 3 GNU General Public License as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -23,16 +9,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Store our compass as a variable.
 HMC5883L compass;
-// Record any errors that may occur in the compass.
+// Record any errors that may occur.
 int error = 0;
+
+const int sample = 5;
+int initFlag=0;
+float initHeading=0;
+float avg[sample];
 
 // Out setup routine, here we will configure the microcontroller and compass.
 void setup()
 {
   // Initialize the serial port.
   Serial.begin(9600);
-
-  Serial.println("Starting the I2C interface.");
+  Serial.flush();
+  
+  Serial.println("\nStarting the I2C interface.");
   Wire.begin(); // Start the I2C interface.
 
   Serial.println("Constructing new HMC5883L");
@@ -52,65 +44,69 @@ void setup()
 // Our main program loop.
 void loop()
 {
-  // Retrive the raw values from the compass (not scaled).
-  MagnetometerRaw raw = compass.ReadRawAxis();
   // Retrived the scaled values from the compass (scaled to the configured scale).
   MagnetometerScaled scaled = compass.ReadScaledAxis();
   
-  // Values are accessed like so:
-  int MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
-
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
   float heading = atan2(scaled.YAxis, scaled.XAxis);
   
   // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: 2� 37' W, which is 2.617 Degrees, or (which we need) 0.0456752665 radians, I will use 0.0457
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
+  // Find yours here: http://www.magnetic-declination.com/ If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
   float declinationAngle = 0.01367;
   heading += declinationAngle;
   
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * (180/M_PI); 
+  
+  // Check for initial reading
+  if(initFlag==0) {
+    initHeading = headingDegrees;
+    initFlag = 1;
+  }
+  else
+    // Convert all subsequent readings to be relative to the initial reading.
+    headingDegrees -= initHeading;
+
   // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
+  if(headingDegrees < 0)
+    headingDegrees += 360;
     
   // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
+  if(headingDegrees > 360)
+    headingDegrees -= 360;
    
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-
   // Output the data via the serial port.
-  Output(raw, scaled, heading, headingDegrees);
+  Output(headingDegrees);
 
   // Normally we would delay the application by 66ms to allow the loop
   // to run at 15Hz (default bandwidth for the HMC5883L).
-  // However since we have a long serial out (104ms at 9600) we will let
+  // However, since we have a long serial out (104ms at 9600), we will let 
   // it run at its natural speed.
-  delay(66);
+  delay(500);
 }
 
 // Output the data down the serial port.
-void Output(MagnetometerRaw raw, MagnetometerScaled scaled, float heading, float headingDegrees)
+void Output(float headingDegrees)
 {
-   Serial.print("Raw:\t");
-   Serial.print(raw.XAxis);
-   Serial.print("   ");   
-   Serial.print(raw.YAxis);
-   Serial.print("   ");   
-   Serial.print(raw.ZAxis);
-   Serial.print("   \tScaled:\t");
-   
-   Serial.print(scaled.XAxis);
-   Serial.print("   ");   
-   Serial.print(scaled.YAxis);
-   Serial.print("   ");   
-   Serial.print(scaled.ZAxis);
-
    Serial.print("   \tHeading:\t");
-   Serial.print(heading);
-   Serial.print(" Radians   \t");
    Serial.print(headingDegrees);
+   Serial.print(" Degrees   \t");
+   
+   int pos = CalcAvg(headingDegrees);
+   Serial.print("   \tAverage:\t");
+   Serial.print(pos);
    Serial.println(" Degrees   \t");
+}
+
+float CalcAvg(float headingDegrees)
+{
+   float total= headingDegrees;
+   
+   for(int i= 0; i<sample-1; i++){
+       total += avg[i];
+       avg[i]=avg[i+1];
+   }
+   avg[sample-1] = headingDegrees;
+   
+   return (total/sample);
 }
